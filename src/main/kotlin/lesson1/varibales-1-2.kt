017b6d73f8c22114a305bf24b1ed12bb7a3b3810
Forren <forrenzxc@gmail.com>
2025-12-01 13:53:32 +0400

    varibales-1-2

---
+++ src/main/kotlin/lesson1/varibales-1-2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val orderCount = 75
+    val message = "Спасибо за покупку!"
+
+    println(orderCount)
+    println(message)
+
+    var workers = 2000
+//    println(workers)
+    workers--
+    println(workers)
+}
