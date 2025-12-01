bdc163143dd9eb3a3ad8348e5653550d5a436948
Forren <forrenzxc@gmail.com>
2025-12-01 23:31:29 +0400

    KS-1-3 bugfix-2

--- src/main/kotlin/lesson1/KS-1-3.kt
+++ src/main/kotlin/lesson1/KS-1-3.kt
@@ -1,14 +1,14 @@
 fun main() {
     val year: Int = 1961
 
-    var hours: Int = 9
-    var minutes: Int = 7
+    var hours: String = "09"
+    var minutes: String = "07"
 
     println(year)
-    println("%02d".format(hours))
-    println("%02d".format(minutes))
+    println(hours)
+    println(minutes)
 
-    hours = 10
-    minutes = 55
-    println("%02d:%02d".format(hours, minutes))
+    hours = "10"
+    minutes = "55"
+    println("$hours:$minutes")
 }
