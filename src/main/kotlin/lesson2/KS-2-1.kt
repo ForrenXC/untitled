117dfa60011d1610cd6265bc65876e67116a2ad7
Forren <forrenzxc@gmail.com>
2025-12-02 13:51:58 +0400

    KS-2-1

---
+++ src/main/kotlin/lesson2/KS-2-1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson2
+
+fun  main() {
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+
+    val average  = (student1 + student2 + student3 + student4) / 4f
+    println("%.2f".format(average))
+}
